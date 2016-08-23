5df3c153a41b0ec8933d76ae42293b849da91332
bobotjones <erin@sockpuppet.org>
2016-08-23 18:14:37 -0500

    WE ARE THE CHAMPIONS

--- src/set2/twelve/twelve.go
+++ src/set2/twelve/twelve.go
@@ -12,16 +12,14 @@ var unString = util.Decode64("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd2
 var altString = unString
 
 func makeadict(in []byte, c []byte) [][]byte {
-	//fmt.Printf("Got %d bytes\n", len(in))
 	ret := make([][]byte, 0)
-	for i := 33; i < 126; i++ {
+	for i := 10; i < 126; i++ {
 		fill := make([]byte, 0)
 		fill = append(fill, in...)
 		for _, v := range c {
 			fill = append(fill, v)
 		}
 		fill = append(fill, byte(i))
-		//fmt.Printf("%d in: %q\n", len(fill), fill)
 		fin := myFunc(fill, unKey)
 		ret = append(ret, fin)
 	}
@@ -29,23 +27,18 @@ func makeadict(in []byte, c []byte) [][]byte {
 }
 
 func myFunc(src, key []byte) []byte {
-	//fmt.Println("IN  ", string(src))
 	// unknown-string
-
 	src = append(src, altString...)
 	// pad
-	fmt.Println(string(src))
 	if len(src)%16 != 0 {
 		pddr := util.NewPadder(16)
 		pddr.Data.Write(src)
 		pddr.Padfoot()
 		src = pddr.Data.Bytes()
 	}
-	//fmt.Printf("%q\n", src)
 	out := make([]byte, len(src))
 	ecb := util.NewECB(key)
 	ecb.Encrypt(out, src)
-	//fmt.Printf("inter\t%v\n", out[:16])
 	return out
 }
 
@@ -53,7 +46,6 @@ func gbs(key []byte) int {
 	var i, score, bs int
 	for i = 1; i <= 128; i++ {
 		x := strings.Repeat("A", i)
-		//fmt.Println("Input %d\t%s\n", i, x)
 		src := make([]byte, 0)
 		src = append(src, []byte(x)...)
 		out := myFunc(src, key)
@@ -69,9 +61,7 @@ func gbs(key []byte) int {
 func main() {
 	unKey = util.RandString(16)
 	bs := gbs(unKey)
-	//fmt.Printf("Found block size %d\n", bs)
 	o := bs - 1
-
 	in := []byte(strings.Repeat("A", o))
 	out := myFunc(in, unKey)
 	dec := make([]byte, 0)
@@ -79,14 +69,10 @@ func main() {
 
 	for len(dec) != len(unString) {
 		for k, v := range dict {
-			//fmt.Printf("%q\n", (k + 33))
-			//fmt.Printf("%d\t%v\n", len(v[:bs]), v[:bs])
-			//fmt.Printf("%d\t%v\n", len(out[:bs]), out[:bs])
 			if bytes.Equal(v[:bs], out[:bs]) {
-				c := byte(k + 33)
+				c := byte(k + 10)
 				dec = append(dec, c)
-				fmt.Printf("MOOSE\t%q\n", dec)
-				//o--
+				fmt.Printf("%q\n", dec)
 				altString = unString[len(dec):]
 				in = []byte(strings.Repeat("A", o))
 				makeadict(in, dec)
