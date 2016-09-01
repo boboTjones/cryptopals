3866f0bc8fa646af62964138c10358b840d69c16
bobotjones <erin@sockpuppet.org>
2016-08-31 23:14:14 -0500

    This produces hilarious output. I'm done for today. Bye, August!

--- src/cc/util/ecb.go
+++ src/cc/util/ecb.go
@@ -3,11 +3,13 @@ package util
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"fmt"
 )
 
 type ECB struct {
 	thing     cipher.Block
 	blockSize int
+	key       []byte
 }
 
 func NewECB(key []byte) *ECB {
@@ -36,4 +38,35 @@ func (e *ECB) Decrypt(dst, src []byte) {
 
 func (e *ECB) Blocksize() int {
 	return e.blockSize
+}
+
+func (e *ECB) Dict(rp, ac, tb, c []byte) [][]byte {
+	fmt.Println("Make dict")
+	ret := make([][]byte, 0)
+	for i := 10; i < 126; i++ {
+		fill := make([]byte, 0)
+		fill = append(fill, rp...)
+		fill = append(fill, ac...)
+		fill = append(fill, c...)
+		fill = append(fill, byte(i))
+		fill = append(fill, tb...)
+		fmt.Printf("Fill\t%q\n", fill)
+		fin := e.Orakkl(fill)
+		ret = append(ret, fin)
+	}
+	return ret
+}
+
+func (e *ECB) Orakkl(src []byte) []byte {
+	fmt.Println("Oracling")
+	if len(src)%16 != 0 {
+		pddr := NewPadder(16)
+		pddr.Data.Write(src)
+		pddr.Padfoot()
+		src = pddr.Data.Bytes()
+	}
+	fmt.Printf("Padd\t%q\n", src)
+	out := make([]byte, len(src))
+	e.thing.Encrypt(out, src)
+	return out
 }
--- src/set2/fourteen/fourteen.go
+++ src/set2/fourteen/fourteen.go
@@ -2,29 +2,57 @@
 package main
 
 import (
+	"bytes"
+	"cc/util"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-// Take your oracle function from #12.
-
-func myFunc(src, key []byte) []byte {
-	// unknown-string
-	src = append(src, altString...)
-	// pad
-	if len(src)%16 != 0 {
-		pddr := util.NewPadder(16)
-		pddr.Data.Write(src)
-		pddr.Padfoot()
-		src = pddr.Data.Bytes()
-	}
-	out := make([]byte, len(src))
-	ecb := util.NewECB(key)
-	ecb.Encrypt(out, src)
-	return out
+func mkSrc(rp, ac, tb []byte) []byte {
+	src := make([]byte, 0)
+	src = append(src, rp...)
+	src = append(src, ac...)
+	src = append(src, tb...)
+	return src
 }
 
 func main() {
+	key := util.RandString(16)
 	//Now generate a random count of random bytes and prepend this string to every plaintext.
 	// AES-128-ECB(random-prefix || attacker-controlled || target-bytes, random-key)
-	fmt.Println("helu")
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	// random-prefix
+	rp := util.RandString(r1.Intn(33) + 2)
+	// attacker-controlled
+	ac := bytes.Repeat([]byte("A"), 15)
+	// target-bytes
+	tb := util.Decode64("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
+	spare := tb
+
+	in := mkSrc(rp, ac, spare)
+	ecb := util.NewECB(key)
+	out := ecb.Orakkl(in)
+
+	dec := make([]byte, 0)
+	dict := ecb.Dict(rp, ac, tb, dec)
+	fmt.Println("WTF ", dict[len(dict)-1:])
+
+	for len(dec) != len(tb) {
+		for k, v := range dict {
+			fmt.Printf("dict\t%v\n", v[:16])
+			fmt.Printf("out \t%v\n", out[:16])
+			if bytes.Equal(v[:16], out[:16]) {
+				dec = append(dec, byte(k+10))
+				fmt.Printf("Found %q\n", dec)
+				spare = spare[len(dec):]
+				in = mkSrc(rp, ac, spare)
+				dict = ecb.Dict(rp, ac, tb, dec)
+				out = ecb.Orakkl(in)
+				break
+			}
+		}
+	}
+	fmt.Println(string(dec))
 }
